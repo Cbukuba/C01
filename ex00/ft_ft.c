70adff1ab853cba29fb4c9e3be0a52acab19e129
Cbukuba <chrisbukuba058@gmail.com>
2021-09-09 15:57:53 +0200

    Exercise00

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -6,11 +6,19 @@
 /*   By: cbukuba <chrisbukuba058@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 14:28:06 by cbukuba           #+#    #+#             */
-/*   Updated: 2021/09/09 14:37:50 by cbukuba          ###   ########.fr       */
+/*   Updated: 2021/09/09 15:55:20 by cbukuba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_ft(int *nbr)
 {
-	nbr = 42;
+	*nbr = 42;
 }
+
+/*int main()
+{
+	int	nb;
+	ft_ft(&nb);
+
+	printf("%d", nb);
+}*/
