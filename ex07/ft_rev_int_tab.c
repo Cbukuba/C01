f8aa5f4a53f42d259ac6b94b768caf8ced1a83a8
cbukuba <cbukuba@student.s19.be>
2021-09-14 20:25:47 +0000

    Exercise07

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -13,12 +13,28 @@
 void	ft_rev_int_tab(int *tab, int size)
 {
 	int	i;
+	int	y;
 
 	i = 0;
-	while (i < size)
+	while (i <= size)
 	{
+		y = tab[i];
 		tab[i] = tab[size - 1];
+		tab [size - 1] = y;
 		i ++;
 		size --;
 	}
 }
+
+/*int	main()
+{
+	int tab [5];
+	tab [0] = 1;
+	tab [1] = 2;
+	tab [2] = 3;
+	tab [3] = 4;
+	tab [4] = 5;
+	int size = 5;
+	ft_rev_int_tab(tab, size);
+	printf("%d,%d,%d,%d,%d", tab[0], tab[1], tab[2], tab[3], tab[4]);
+}*/
