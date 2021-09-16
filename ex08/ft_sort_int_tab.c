22de254837e3e2b0172b731ba0a3c0baa96bc502
cbukuba <cbukuba@student.s19.be>
2021-09-16 17:53:18 +0000

    Tantpiszebi

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -14,29 +14,36 @@ void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
 	int	y;
+	int c;
 
+	c = 0;
 	i = 0;
-	while (i <= size)
+	while (c++ < size)
 	{
-		y = 0;
-		if (tab[i] > tab[i + 1])
+		while (i < size)
 		{
-			y = tab[i];
-			tab[i] = tab[i + 1];
-			tab[i + 1] = y;
+			y = 0;
+			if (tab[i] > tab[i + 1])
+			{
+				y = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = y;
+			}
+			i ++;
 		}
-		i ++;
+		c ++;
 	}
 }
 
-/*int	main()
+/*#include <stdio.h>
+int	main()
 {
 	int tab [5];
 	tab [0] = 1;
-	tab [1] = 6;
+	tab [1] = 5;
 	tab [2] = 4;
 	tab [3] = 3;
-	tab [4] = 5;
+	tab [4] = 2;
 	int size = 5;
 	ft_sort_int_tab(tab, size);
 	printf("%d,%d,%d,%d,%d", tab[0], tab[1], tab[2], tab[3], tab[4]);
